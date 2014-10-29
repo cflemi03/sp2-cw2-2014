7ef9dd04e8579fb1db9c7c4a397544c94363fb80
= 'cflemi03 <= 'cflemi03@dcs.bbk.ac.uk>
2014-10-29 12:52:52 +0000

    started calculator
    initialised to 0, trying to add steps for calculation

--- FractionCalculator.java
+++ FractionCalculator.java
@@ -1,5 +1,20 @@
 package fractions;
 
+import java.util.Scanner; 
+
 public class FractionCalculator {
 
+	public FractionCalculator() {
+		new Fraction(0,1);
+	}
+	
+	public String calc(String calc, String hold) {
+		Scanner input = new Scanner(System.in);
+		hold = input.next();
+		if (hold.isDigit())
+		if (input.next() == "+") {
+			int sum = hold + input.next();
+		}
+		int num1 = input.nextInt();
+	}
 }
