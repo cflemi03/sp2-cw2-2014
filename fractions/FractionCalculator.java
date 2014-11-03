525714c6b31d2adfc8adef682e972a6b273a2fe0
= 'cflemi03 <= 'cflemi03@dcs.bbk.ac.uk>
2014-11-03 15:04:50 +0000

    added abs, neg, clear

--- fractions/FractionCalculator.java
+++ fractions/FractionCalculator.java
@@ -21,25 +21,36 @@ public class FractionCalculator {
 				sum = hold + (input.next());
 			}
 			else if (input.next() == "-") {
+				subtract sum1 = new subtract();
 				sum = hold - (input.next());
 			}
 			else if (input.next() == "*") {
+				multiply sum1 = new multiply();
 				sum = hold * (input.next());
 			}
 			else if (input.next() == " / ") {
+				divide sum1 = new divide();
 				sum = hold / (input.next());
 			}
 			else if (input.next() == "/") {
-				fractions.Fraction = new Fraction(hold, (input,next())
+				Fraction = new Fraction(hold, (input,next());
 			}
-			else if (input.next() == "a" || 'A') {
-				fractions.Fraction = new Fraction(hold, (input,next())
+			else if (input.next() == "a" || "A") {
+				absValue sum1 = new absValue();
+			}
+			else if (input.next() == "n" || "N") {
+				negate sum1 = new negate();
+			}
+			else if (input.next() == "c" || "N") {
+				sum = 0;
+				new Fraction(0,1);
+			}
+			else {
+				return "0";
 			}
 		}
-	
-	public Fraction evaluate(Fraction frac, String inputString)	{
-		
 	}
-	
+	public Fraction evaluate(Fraction frac, String inputString)	{
+		return frac;
 	}
 }
